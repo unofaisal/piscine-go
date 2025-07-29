95e3c969790f0b7b7592a6e8c17144f752f0e8a1
faimali <faisalmasiga3054@gmail.com>
2025-07-30 02:46:14 +0300

    my family

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-curl -s https://learn.zone01kisumu.ke/assets/superhero/all.json | jq -r ' .[] | select( .id=='$HERO_ID' ) .connections.relatives'
+curl -s https://learn.zone01kisumu.ke/assets/superhero/all.json | jq -c '.[] | select(.id=='"$HERO_ID"') | .connections.relatives' | sed 's/"//g'
