c2c4cc770f3e8a6e8669d11d674f68d1a607e671
faimali <faisalmasiga3054@gmail.com>
2025-07-31 13:41:59 +0300

    print combination 2

---
+++ printcomb2.go
@@ -0,0 +1,24 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	for a := 0; a <= 98; a++ {
+		for b := a + 1; b <= 99; b++ {
+			print2Digits(a)
+			z01.PrintRune(' ')
+			print2Digits(b)
+
+			if !(a == 98 && b == 99) {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
+
+func print2Digits(n int) {
+	z01.PrintRune(rune('0' + n/10))
+	z01.PrintRune(rune('0' + n%10))
+}
