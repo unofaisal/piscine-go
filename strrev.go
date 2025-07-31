e0bffd5a52b9d1d5368aa3253686fc45b54c1658
faimali <faisalmasiga3054@gmail.com>
2025-08-01 00:47:55 +0300

    string reverse

--- strrev.go
+++ strrev.go
@@ -3,7 +3,7 @@ package piscine
 func StrRev(s string) string {
 	r := []rune(s)
 	var x string
-	for i := len(r); i >= 0; i-- {
+	for i := len(r) - 1; i >= 0; i-- {
 		x += string(r[i])
 	}
 	return x
