1ee6afb0f6534f00520cc3aa8a094714b35542d0
Christopher Fremond <dev@01-edu.org>
2025-07-31 10:02:45 +0000

    Update printcomb.go

--- printcomb.go
+++ printcomb.go
@@ -6,18 +6,17 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
    for a := '0'; a <= '7'; a++{
-     for b := (a + 1); b <= '8'; b++{
-       for c := (b + 1); c <= '9'; c++{
-         z01.PrintRune(a)
-         z01.PrintRune(b)
-         z01.PrintRune(c)
+     for b := (a + '1'); b <= '8'; b++{
+       for c := (b + '1'); c <= '9'; c++{
+         z01.PrintRune('0' + a)
+         z01.PrintRune('0' + b)
+         z01.PrintRune('0' + c)
          if !(a=='7' && b=='8' && c=='9'){
            z01.PrintRune(',')
            z01.PrintRune(' ')
          }
        }
      }
-     z01.PrintRune('\n')
-     
    }
+  z01.PrintRune('\n')
 }
