30c72e4e30f2723d491ed9efa00556372c012a39
Faisal Imali <faimali@noreply.learn.zone01kisumu.ke>
2025-08-02 10:55:52 +0000

    Update sortintegertable.go

--- sortintegertable.go
+++ sortintegertable.go
@@ -3,7 +3,7 @@ package piscine
 func SortIntegerTable(table []int) {
 	for range table {
 		for i, c := range table {
-			if i < len(table)-1 && c > table[i+1] {
+			if i != (len(table)-1) && c > table[i+1] {
 				table[i], table[i+1] = table[i+1], table[i]
 			}
 		}
