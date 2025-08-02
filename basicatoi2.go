a770607824217dd8ff0e4ef25c509089bf9c74dd
faimali <faisalmasiga3054@gmail.com>
2025-08-02 09:37:48 +0300

    basic ascii to int

---
+++ basicatoi2.go
@@ -0,0 +1,13 @@
+package piscine
+
+func BasicAtoi2(s string) int {
+	var x int
+	for _, i := range s {
+		if !(i >= '0' && i <= '9') {
+			return 0
+		} else {
+			x += x*10 + int(i-'0')
+		}
+	}
+	return x
+}
