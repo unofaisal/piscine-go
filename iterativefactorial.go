9e44243920e03d1d573bb443cc96ef5eea796aad
faimali <faisalmasiga3054@gmail.com>
2025-08-05 13:20:03 +0300

    iterative factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,16 +4,9 @@ func IterativeFactorial(nb int) int {
 	if nb < 0 {
 		return 0
 	}
-	if nb == 0 || nb == 1 {
-		return 1
-	}
 	result := 1
-	for nb > 0 {
-		result = result * nb
-		nb--
-	}
-	if result < 0 {
-		return 0
+	for i := 2; i <= nb; i++ {
+		result *= i
 	}
 	return result
 }
