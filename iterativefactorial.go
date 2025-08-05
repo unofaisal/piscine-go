5226cbb112a97dfb4c468af5cd71af7513c8b9fa
faimali <faisalmasiga3054@gmail.com>
2025-08-05 13:13:03 +0300

    iterative factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,10 +1,19 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
+	if nb < 0 {
+		return 0
+	}
+	if nb == 0 || nb == 1 {
+		return 1
+	}
 	result := 1
 	for nb > 0 {
 		result = result * nb
 		nb--
+	}
+	if result < 0 {
+		return 0
 	}
 	return result
 }
