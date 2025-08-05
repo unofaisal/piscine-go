71039db9305e4ca0011542c3465e05595308819a
faimali <faisalmasiga3054@gmail.com>
2025-08-05 20:49:55 +0300

    update

--- lastrune.go
+++ lastrune.go
@@ -1,12 +1,12 @@
 package piscine
 
 func LastRune(s string) rune {
-  r := []rune(s)
-  var x rune
-  for i, c := range r {
-    if i == len(s) - 1 {
-      x = c
-    }
-  }
-  return x
+	r := []rune(s)
+	var x rune
+	for i, c := range r {
+		if i == len(s)-1 {
+			x = c
+		}
+	}
+	return x
 }
