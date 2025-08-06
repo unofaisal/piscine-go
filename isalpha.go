9a34b11a1986904e9523e509e4f3dd28e226f850
faimali <faisalmasiga3054@gmail.com>
2025-08-06 17:58:42 +0300

    is alpha

--- isalpha.go
+++ isalpha.go
@@ -1,10 +1,11 @@
 package piscine
 
 func IsAlpha(s string) bool {
+	x := true
 	for _, c := range s {
-		if !(c >= '0' && c <= '9') {
-			return false
+		if !(c >= '0' && c <= '9') && !(c >= 'a' && c <= 'z') && !(c >= 'A' && c <= 'Z') {
+			x = false
 		}
 	}
-	return true
+	return x
 }
