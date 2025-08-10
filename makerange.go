f40ee164823271fbe9749b25c8d47f535d5a4859
Faisal Imali <faimali@noreply.learn.zone01kisumu.ke>
2025-08-10 04:40:21 +0000

    make range

---
+++ makerange.go
@@ -0,0 +1,16 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+
+	size := max - min
+
+	fin2 := make([]int, 0)
+	if min > max {
+		return fin2
+	}
+	fin := make([]int, size)
+	for i := range fin {
+		fin[i] = min + i
+	}
+	return fin
+}
