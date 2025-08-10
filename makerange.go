01508bd871e0291177e5d5ea42c9818dfaf7873d
Faisal Imali <faimali@noreply.learn.zone01kisumu.ke>
2025-08-10 04:52:13 +0000

    Update makerange.go

--- makerange.go
+++ makerange.go
@@ -1,13 +1,13 @@
 package piscine
 
 func MakeRange(min, max int) []int {
-    if min >= max {
-        return nil
-    }
-    size := max - min
-    fin := make([]int, size)
-    for i := range fin {
-        fin[i] = min + i
-    }
-    return fin
+	if min >= max {
+		return nil
+	}
+	size := max - min
+	fin := make([]int, size)
+	for i := range fin {
+		fin[i] = min + i
+	}
+	return fin
 }
