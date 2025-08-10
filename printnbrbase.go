e5db276d352051d603cd0eae211371aa009ed862
Faisal Imali <faimali@noreply.learn.zone01kisumu.ke>
2025-08-10 04:16:46 +0000

    code format

--- printnbrbase.go
+++ printnbrbase.go
@@ -1,25 +1,25 @@
 package piscine
 
 func PrintNbrBase(nbr int, base string) {
-    r := []rune(string)
-    pos := []int {}
+	r := []rune(string)
+	pos := []int{}
 
-    result := nbr
-  outPut := ""
-  neg := false
-  if nbr < 0 {
-    nbr = nbr * -1
-    neg = true
-  }
+	result := nbr
+	outPut := ""
+	neg := false
+	if nbr < 0 {
+		nbr = nbr * -1
+		neg = true
+	}
 
-    for result >= 0 {
-      pos = append(pos, result%len(r)-1)
-      result /= len(r)
-    }
+	for result >= 0 {
+		pos = append(pos, result%len(r)-1)
+		result /= len(r)
+	}
 
-  for i := len(pos) - 1; i >= 0; i-- {
-      outPut += r[i]
-  }
-  return output
-    
+	for i := len(pos) - 1; i >= 0; i-- {
+		outPut += r[i]
+	}
+	return output
+
 }
