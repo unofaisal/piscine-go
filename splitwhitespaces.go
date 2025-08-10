4cca6507a7817dfb5f34fc5508d6533f577acd5e
Faisal Imali <faimali@noreply.learn.zone01kisumu.ke>
2025-08-10 05:19:18 +0000

    Update splitwhitespaces.go

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,21 +1,18 @@
 package piscine
 
 func SplitWhiteSpaces(s string) []string {
-	word := ""
-	sl := []string{}
-	for i, l := range s {
-
-		if l >= ' ' && l <= '~' {
-			word += string(l)
-		}
-		if word[len(word)-1] == ' ' || word[len(word)-1] == '	' || word[len(word)-1] == '\n' {
-			sl = append(sl, word[:len(word)-1])
-			word = ""
-
-		}
-		if i == len(s)-1 && word[:] != ' ' || word[:] != '	' || word[:] != '\n' {
-			sl = append(sl, word[:])
- 		}
-	}
-	return sl
+    word := ""
+    sl := []string{}
+    for _, l := range s {
+        if l != ' ' && l != '\t' && l != '\n' {
+            word += string(l)
+        } else if word != "" {
+            sl = append(sl, word)
+            word = ""
+        }
+    }
+    if word != "" {
+        sl = append(sl, word)
+    }
+    return sl
 }
