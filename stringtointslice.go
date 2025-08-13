68570f1313b352ffbfac3ac187239f977039c85c
Faisal Imali <faimali@noreply.learn.zone01kisumu.ke>
2025-08-13 16:17:18 +0000

    Update stringtointslice.go

--- stringtointslice.go
+++ stringtointslice.go
@@ -2,9 +2,11 @@ package piscine
 
 func StringToIntSlice(str string) []int {
 	// by := []byte(str)
-	in := []int{}
-	for _, b := range str {
-		in = append(in, int(b))
+	in := []int(nil)
+	if len(str) > 0 {
+		for _, b := range str {
+			in = append(in, int(b))
+		}
 	}
 	return in
 }
