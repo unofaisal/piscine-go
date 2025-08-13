5db7e4120cb113a07372e61755ac29f10988e190
Faisal Imali <faimali@noreply.learn.zone01kisumu.ke>
2025-08-13 14:59:12 +0000

    Add dealapackofcards.go

---
+++ dealapackofcards.go
@@ -0,0 +1,21 @@
+package rectangle
+
+import "fmt"
+
+func DealAPackOfCards(deck []int) {
+	players := 4
+	cardsPerPlayer := 3
+
+	for i := 0; i < players; i++ {
+		fmt.Printf("Player %d: ", i+1)
+		for j := 0; j < cardsPerPlayer; j++ {
+			card := deck[(cardsPerPlayer*i)+j]
+			if j == cardsPerPlayer-1 {
+				fmt.Printf("%d", card)
+			} else {
+				fmt.Printf("%d, ", card)
+			}
+		}
+		fmt.Println()
+	}
+}
