3432e7560bffdd757265118311d8451d29ef6b2c
Faisal Imali <faimali@noreply.learn.zone01kisumu.ke>
2025-08-13 15:51:30 +0000

    Update dealapackofcards.go

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -3,19 +3,28 @@ package piscine
 import "fmt"
 
 func DealAPackOfCards(deck []int) {
-	players := 4
-	cardsPerPlayer := 3
+	mp := make(map[int][]int)
+	i := 0
+	x := 1
+	for i <= len(deck)-1 {
 
-	for i := 0; i < players; i++ {
-		fmt.Printf("Player %d: ", i+1)
-		for j := 0; j < cardsPerPlayer; j++ {
-			card := deck[(cardsPerPlayer*i)+j]
-			if j == cardsPerPlayer-1 {
-				fmt.Printf("%d", card)
-			} else {
-				fmt.Printf("%d, ", card)
+		// fmt.Printf("hello Player %v: %v", x, mp[x])
+		if i >= 2 && i%3 == 0 {
+			x++
+		}
+		mp[x] = append(mp[x], deck[i])
+		i++
+	}
+	for k, v := range mp {
+
+		fmt.Printf("Player %v:", k)
+		for i, in := range v {
+			fmt.Printf(" %v", in)
+			if i < len(v)-1 {
+				fmt.Printf(",")
 			}
 		}
-		fmt.Println()
+		fmt.Printf("\n")
+
 	}
 }
