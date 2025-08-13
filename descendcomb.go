28b8a7dbb923f9d1245f08d09c1aae40f5885e9d
Faisal Imali <faimali@noreply.learn.zone01kisumu.ke>
2025-08-13 22:42:52 +0000

    Update descendcomb.go

--- descendcomb.go
+++ descendcomb.go
@@ -1,23 +1,24 @@
 package piscine
+
 import "github.com/01-edu/z01"
 
 func DescendComb() {
-  for i := 99; i >= 1; i-- {
-    for x := i - 1; x >= 0; x-- {
-      print2dig(i)
-      z01.PrintRune(' ')
-      print2dig(x)
-      if !(i == 1 && x == 0) {
-        z01.PrintRune(',')
-      z01.PrintRune(' ')
-      // fmt.Print(i, " ", x, ", ")
-      }
-      
-    }
-  }
+	for i := 99; i >= 1; i-- {
+		for x := i - 1; x >= 0; x-- {
+			print2dig(i)
+			z01.PrintRune(' ')
+			print2dig(x)
+			if !(i == 1 && x == 0) {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+				// fmt.Print(i, " ", x, ", ")
+			}
+
+		}
+	}
 }
 
 func print2dig(in int) {
-  z01.PrintRune(rune('0' + in/10))
-  z01.PrintRune(rune('0' + in%10))
+	z01.PrintRune(rune('0' + in/10))
+	z01.PrintRune(rune('0' + in%10))
 }
