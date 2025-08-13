477a46b61ad900e2adbe39c907587119b423757b
Faisal Imali <faimali@noreply.learn.zone01kisumu.ke>
2025-08-13 04:34:55 +0000

    update

--- point/main.go
+++ point/main.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package piscine
 
 type point struct {
 	x int
@@ -10,12 +8,4 @@ type point struct {
 func setPoint(ptr *point) {
 	ptr.x = 42
 	ptr.y = 21
-}
-
-func main() {
-	points := &point{}
-
-	setPoint(points)
-
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
 }
