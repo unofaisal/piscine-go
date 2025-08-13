27e3346106765d0eb5d5734a6fad63080a9c2338
Faisal Imali <faimali@noreply.learn.zone01kisumu.ke>
2025-08-13 05:04:33 +0000

    update

--- point/main.go
+++ point/main.go
@@ -12,30 +12,50 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
-func printStr(s string) {
-	for _, r := range s {
-		z01.PrintRune(r)
-	}
-}
+func main() {
+	points := &point{}
+	setPoint(points)
 
-func printNbr(n int) {
+	// Print "x = "
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+
+	// print points.x
+	n := points.x
 	if n < 0 {
 		z01.PrintRune('-')
 		n = -n
 	}
 	if n >= 10 {
-		printNbr(n / 10)
+		z01.PrintRune('0' + n/10)
+		z01.PrintRune('0' + n%10)
+	} else {
+		z01.PrintRune('0' + n)
 	}
-	z01.PrintRune(rune(n%10 + '0'))
-}
 
-func main() {
-	points := &point{}
-	setPoint(points)
+	// Print ", y = "
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
 
-	printStr("x = ")
-	printNbr(points.x)
-	printStr(", y = ")
-	printNbr(points.y)
+	// print points.y
+	m := points.y
+	if m < 0 {
+		z01.PrintRune('-')
+		m = -m
+	}
+	if m >= 10 {
+		z01.PrintRune('0' + m/10)
+		z01.PrintRune('0' + m%10)
+	} else {
+		z01.PrintRune('0' + m)
+	}
+
+	// newline
 	z01.PrintRune('\n')
 }
