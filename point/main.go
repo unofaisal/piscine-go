3ea14a38e5121b12ebcf4f9850dcda225b971d24
Faisal Imali <faimali@noreply.learn.zone01kisumu.ke>
2025-08-13 05:05:52 +0000

    update

--- point/main.go
+++ point/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/01-edu/z01"
+import "fmt"
 
 type point struct {
 	x int
@@ -14,48 +14,8 @@ func setPoint(ptr *point) {
 
 func main() {
 	points := &point{}
+
 	setPoint(points)
 
-	// Print "x = "
-	z01.PrintRune('x')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
-
-	// print points.x
-	n := points.x
-	if n < 0 {
-		z01.PrintRune('-')
-		n = -n
-	}
-	if n >= 10 {
-		z01.PrintRune('0' + n/10)
-		z01.PrintRune('0' + n%10)
-	} else {
-		z01.PrintRune('0' + n)
-	}
-
-	// Print ", y = "
-	z01.PrintRune(',')
-	z01.PrintRune(' ')
-	z01.PrintRune('y')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
-
-	// print points.y
-	m := points.y
-	if m < 0 {
-		z01.PrintRune('-')
-		m = -m
-	}
-	if m >= 10 {
-		z01.PrintRune('0' + m/10)
-		z01.PrintRune('0' + m%10)
-	} else {
-		z01.PrintRune('0' + m)
-	}
-
-	// newline
-	z01.PrintRune('\n')
+	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
 }
