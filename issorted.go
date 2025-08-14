d08fd8182cfad876e200458491b70f27786bfba1
faimali <faisalmasiga3054@gmail.com>
2025-08-14 15:56:19 +0300

    issorted.go

---
+++ issorted.go
@@ -0,0 +1,25 @@
+package piscine
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	isSort := false
+	for ind, in := range a {
+		if f(in, a[ind+1]) == 1 {
+			return false
+		} else if f(in, a[ind+1]) == 0 {
+			isSort = true
+		} else {
+			isSort = true
+		}
+	}
+	return isSort
+}
+
+// func gtLt(a, b int) int {
+// 	if a > b {
+// 		return 1
+// 	} else if a < b {
+// 		return 0
+// 	} else {
+// 		return -1
+// 	}
+// }
