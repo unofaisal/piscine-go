394cf793dd2055e6bdee2ede1a89238c26eeef6c
Christopher Fremond <dev@01-edu.org>
2025-08-14 15:22:13 +0000

    Add sortwordarr.go

---
+++ sortwordarr.go
@@ -0,0 +1,12 @@
+package piscine
+
+
+func SortWordArr(a []string) {
+  for range a {
+    for ind, str := range a {
+    if ind < len(a) - 1 && str > a[ind + 1] {
+      a[ind], a[ind+1] = a[ind+1], a[ind]
+    }
+  }
+  }
+}
