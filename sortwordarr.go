d2f9e3fd3b549ab9f8d5528fcd3d1a0c92247756
Faisal Imali <faimali@noreply.learn.zone01kisumu.ke>
2025-08-14 15:25:06 +0000

    Update sortwordarr.go

--- sortwordarr.go
+++ sortwordarr.go
@@ -1,12 +1,11 @@
 package piscine
 
-
 func SortWordArr(a []string) {
-  for range a {
-    for ind, str := range a {
-    if ind < len(a) - 1 && str > a[ind + 1] {
-      a[ind], a[ind+1] = a[ind+1], a[ind]
-    }
-  }
-  }
+	for range a {
+		for ind, str := range a {
+			if ind < len(a)-1 && str > a[ind+1] {
+				a[ind], a[ind+1] = a[ind+1], a[ind]
+			}
+		}
+	}
 }
