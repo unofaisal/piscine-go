66d3967ececbf2ddb03eca9c1bf7751cb2fe54e1
faimali <faisalmasiga3054@gmail.com>
2025-08-16 11:42:35 +0300

    unmatch

--- unmatch.go
+++ unmatch.go
@@ -1,12 +1,12 @@
 package piscine
 
 func Unmatch(a []int) int {
-	mp := make(map[int]int)
+	mpC := make(map[int]int)
 
 	for _, in := range a {
-		mp[in] += 1
+		mpC[in] += 1
 	}
-	for key, val := range mp {
+	for key, val := range mpC {
 		if val == 1 || val%2 > 0 {
 			return key
 		}
