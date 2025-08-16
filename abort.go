b8ef06dd53d6ff8608e76d7073a8632fb0be130c
faimali <faisalmasiga3054@gmail.com>
2025-08-16 16:28:14 +0300

    abort

---
+++ abort.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Abort(a, b, c, d, e int) int {
+	sl := []int{a, b, c, d, e}
+	for range sl {
+		for i, in := range sl {
+			if in > sl[i+1] {
+				sl[i], sl[i+1] = sl[i+1], sl[i]
+			}
+		}
+	}
+	return sl[len(sl)/2]
+}
