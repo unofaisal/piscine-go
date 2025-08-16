1789e16a800fe6781e13c2f06f8b8575750d0e6a
faimali <faisalmasiga3054@gmail.com>
2025-08-16 16:20:24 +0300

    collatz countdown

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -2,7 +2,10 @@ package piscine
 
 func CollatzCountdown(start int) int {
 	if start == 1 {
-		return 1
+		return 0
+	}
+	if start <= 0 {
+		return -1
 	}
 	if start%2 == 0 {
 		return 1 + CollatzCountdown(start/2)
