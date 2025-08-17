7e471aa8b7f177c132cae4c9fcf1f7c3f5e4d4d1
faimali <faisalmasiga3054@gmail.com>
2025-08-17 12:33:07 +0300

    podium position

--- podiumposition.go
+++ podiumposition.go
@@ -1,6 +1,9 @@
 package piscine
 
 func PodiumPosition(podium [][]string) [][]string {
+	if len(podium) == 0 {
+		return podium
+	}
 	for range podium {
 		for i, no := range podium {
 			if i+1 < len(podium) && no[0][0] > podium[i+1][0][0] {
