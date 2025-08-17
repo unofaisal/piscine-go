47c7db4d8439c179e19a34a13c45e9a2acef8677
faimali <faisalmasiga3054@gmail.com>
2025-08-17 15:03:58 +0300

    point

--- point/main.go
+++ point/main.go
@@ -1,10 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"github.com/01-edu/z01"
+)
 
 type point struct {
 	x int
 	y int
+}
+
+func printCh(str string) {
+	for _, c := range str {
+		z01.PrintRune(c)
+	}
+}
+
+func ita(in int) string {
+	str := ""
+	for i := in; i > 0; i /= 10 {
+		str = string('0'+(i%10)) + str
+	}
+	return str
 }
 
 func setPoint(ptr *point) {
@@ -16,6 +32,13 @@ func main() {
 	points := &point{}
 
 	setPoint(points)
+	printCh("x = ")
+	xV := ita(points.x)
+	printCh(xV + ", ")
+	printCh("y = ")
+	yV := ita(points.y)
+	printCh(yV)
+	z01.PrintRune('\n')
 
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	// fmt.Printf("x = %d, y = %d\n", points.x, points.y)
 }
