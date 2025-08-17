419f179d36a5deca57c0c9a3c00900be91e2ca30
faimali <faisalmasiga3054@gmail.com>
2025-08-17 14:23:24 +0300

    loaf of bread

---
+++ loafofbread.go
@@ -0,0 +1,23 @@
+package piscine
+
+func LoafOfBread(str string) string {
+	if len(str) < 5 {
+		return "Invalid Output\n"
+	}
+	// isAppend := true
+	nS := ""
+	counter := 0
+	for i := 0; i < len(str); i++ {
+		if str[i] > ' ' && str[i] <= '~' {
+			nS += string(str[i])
+			counter++
+			if counter%5 == 0 {
+				i++
+				nS += " "
+			}
+
+			// fmt.Println(i+1, i+1%5)
+		}
+	}
+	return nS + "\n"
+}
