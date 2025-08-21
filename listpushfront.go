071aa491a2ac7412fc8106951a94d9a1647d590d
faimali <faisalmasiga3054@gmail.com>
2025-08-21 15:26:10 +0300

    update

--- listpushfront.go
+++ listpushfront.go
@@ -2,6 +2,11 @@ package piscine
 
 func ListPushFront(l *List, data interface{}) {
 	nL := &NodeL{Data: data}
-	l.Tail.Next = nL
-	l.Tail = nL
+	if l.Head == nil {
+		l.Head = nL
+		l.Tail = nL
+	} else {
+		nL.Next = l.Head
+		l.Head = nL
+	}
 }
