3a51a602464c53439a480fd343b577a3dd08a3a5
faimali <faisalmasiga3054@gmail.com>
2025-08-21 15:42:59 +0300

    update

--- listsize.go
+++ listsize.go
@@ -1,31 +1,10 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
-func ListPushFront(l *List, data interface{}) {
-	nL := &NodeL{Data: data}
-	if l.Head == nil {
-		l.Head = nL
-		l.Tail = nL
-	} else {
-		nL.Next = l.Head
-		l.Head = nL
-	}
-}
-
 func ListSize(l *List) int {
-  counter := 0
-  for l.Head != nil {
-    l.Head = l.Head.Next
-    counter++
-  }
-  return counter
+	counter := 0
+	for l.Head != nil {
+		l.Head = l.Head.Next
+		counter++
+	}
+	return counter
 }
