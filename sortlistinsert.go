e0b238c4f6504392e97219b3fb14c3991aa6a639
Faisal Imali <faimali@noreply.learn.zone01kisumu.ke>
2025-08-22 05:30:41 +0000

    update

--- sortlistinsert.go
+++ sortlistinsert.go
@@ -1,22 +1,19 @@
 package piscine
 
 func SortListInsert(l *NodeI, data_ref int) *NodeI {
-    newNode := &NodeI{Data: data_ref}
+	newNode := &NodeI{Data: data_ref}
 
-    // Case 1: empty list or new node goes before the head
-    if l == nil || data_ref < l.Data {
-        newNode.Next = l
-        return newNode
-    }
+	if l == nil || data_ref < l.Data {
+		newNode.Next = l
+		return newNode
+	}
+	current := l
+	for current.Next != nil && current.Next.Data < data_ref {
+		current = current.Next
+	}
 
-    // Case 2: find position in the middle or end
-    current := l
-    for current.Next != nil && current.Next.Data < data_ref {
-        current = current.Next
-    }
+	newNode.Next = current.Next
+	current.Next = newNode
 
-    newNode.Next = current.Next
-    current.Next = newNode
-
-    return l
+	return l
 }
