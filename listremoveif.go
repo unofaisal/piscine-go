e67edffa14e7b04afd6d262a2b97c6e5aa7624f9
Faisal Imali <faimali@noreply.learn.zone01kisumu.ke>
2025-08-22 05:14:06 +0000

    update

--- listremoveif.go
+++ listremoveif.go
@@ -1,26 +1,5 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
-func ListPushBack(l *List, data interface{}) {
-	nL := &NodeL{Data: data}
-	if l.Head == nil {
-		l.Head = nL
-		l.Tail = nL
-	} else {
-		l.Tail.Next = nL
-		l.Tail = nL
-	}
-}
-
 func CompStr(a, b interface{}) bool {
 	return a == b
 }
